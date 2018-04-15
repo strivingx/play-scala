040603ba2ebf92b328a7b7be2375a88063afae11
yuyayun <yuyayun@xiaomi.com>
2018-04-15 08:39:32 +0800

    完善

--- build.sbt
+++ build.sbt
@@ -1,21 +1,4 @@
 name := """play-scala"""
 version := "1.0-SNAPSHOT"
-
-lazy val root = (project in file(".")).enablePlugins(PlayScala)
-
 scalaVersion := "2.11.11"
 
-libraryDependencies += jdbc
-libraryDependencies += cache
-libraryDependencies += ws
-libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % Test
-libraryDependencies ++= Seq(
-  "com.typesafe.slick" %% "slick" % "3.2.1",
-  "com.typesafe.slick" %% "slick-codegen" % "3.2.1"
-)
-libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.27"
-libraryDependencies += "com.typesafe.play" %% "play-mailer" % "6.0.1"
-libraryDependencies += "com.typesafe.play" %% "play-mailer-guice" % "6.0.1"
-libraryDependencies += "org.apache.zookeeper" % "zookeeper" % "3.5.3-beta"
-libraryDependencies += "ai.x" %% "play-json-extensions" % "0.10.0"
-libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.5.3"
---
+++ project/MyBuild.scala
@@ -0,0 +1,64 @@
+import com.typesafe.sbt.packager.archetypes.JavaAppKeys
+import play.routes.compiler.InjectedRoutesGenerator
+import play.sbt.PlayImport.{ws, _}
+import play.sbt.PlayScala
+import play.sbt.routes.RoutesKeys.routesGenerator
+import sbt.Keys._
+import sbt._
+
+
+object MyBuild extends Build with JavaAppKeys {
+
+  object Deps {
+    val slick_version = "3.2.1"
+    val scala_test = "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % Test
+    val zookeeper = "org.apache.zookeeper" % "zookeeper" % "3.5.3-beta"
+    val mysql = "mysql" % "mysql-connector-java" % "5.1.26"
+    val mongodb = "org.mongodb" %% "mongo-java-driver" % "3.6.3"
+    val redis = "redis.clients" % "jedis" % "2.9.0"
+    val play_json = "ai.x" %% "play-json-extensions" % "0.10.0"
+    val akka = "com.typesafe.akka" % "akka-actor_2.11" % "2.5.3"
+
+    val play_mailer = Seq("com.typesafe.play" %% "play-mailer" % "6.0.1",
+      "com.typesafe.play" %% "play-mailer-guice" % "6.0.1"
+    )
+    val slick = Seq(
+      "com.typesafe.slick" %% "slick" % slick_version,
+      "com.typesafe.slick" %% "slick-codegen" % slick_version
+    )
+  }
+
+  lazy val root = (project in file(".")).enablePlugins(PlayScala)
+        .settings(
+          libraryDependencies ++= Seq(
+            jdbc,
+            cache,
+            ws,
+            Deps.mysql,
+//            Deps.mongodb,
+            Deps.redis,
+            Deps.scala_test,
+            Deps.akka,
+            Deps.play_json,
+            Deps.zookeeper
+          ) ++ Deps.slick
+        )
+
+  def playProject(projectName: String) = baseProject(projectName)
+    .enablePlugins(PlayScala)
+    .settings(
+      routesGenerator := InjectedRoutesGenerator
+    )
+
+  def baseProject(projectName: String) = Project(projectName, file(projectName))
+    .settings(
+      name := projectName,
+      libraryDependencies ++= excludedDeps(
+      )
+    )
+
+  def excludedDeps(deps: ModuleID*) = {
+    deps.toSeq
+    //        _.exclude("org.jboss.netty", "netty")
+  }
+}
